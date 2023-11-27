f160ab4da5a1f77bfa67943c0a402bdb44073113
Serghei Gorbulea <gorbulea.s@gmail.com>
2023-11-27 15:58:58 +0200

    Remove duplicated directory

--- build/libs/bot/bot.lib.d.ts
+++
@@ -1,7 +0,0 @@
-import { Telegraf } from "telegraf";
-export declare class Bot {
-    private telegraf;
-    private chatId;
-    constructor(telegraf: Telegraf, chatId: string);
-    sendMessage(message: string): Promise<import("@telegraf/types").Message.TextMessage>;
-}
--- build/libs/file/file.lib.d.ts
+++
@@ -1,8 +0,0 @@
-export type FileDirType = 'founds' | 'sent';
-export declare class File {
-    exists(filename: string, dir: FileDirType): boolean;
-    create(filename: string, dir: FileDirType): void;
-    delete(filename: string, dir: FileDirType): void;
-    move(filename: string): void;
-    private path;
-}
